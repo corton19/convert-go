a6d33f94d64f518b8fd0b7c0fee6f3751cf64f4c
corton19 <55145393+corton19@users.noreply.github.com>
2022-10-16 18:25:44 +0200

    4th iteration of code
    Generalization of code

--- convert-go/main.go
+++ convert-go/main.go
@@ -5,15 +5,20 @@
 // Create a function which can determine array size of an unknown imported array
 // The code should be less hard-coded (i.e., improve nested for loop at 49-61)
 // Q: Is the array going to be 2D? Otherwise, edit/improve it
+//
 
 package main
 
 import (
 	"fmt"
 	"os"
+	"reflect"
+	"strconv"
 
 	"github.com/scritchley/orc"
 )
+
+////******////
 
 func check(e error) {
 	if e != nil {
@@ -21,56 +26,145 @@ func check(e error) {
 	}
 }
 
-func data() {
-	data_to_file := [][]string{
-		{"Name", "City", "Country", "Skills"},
-		{"Rose", "London", "U.K.", "C"},
-		{"Smith", "Austin", "U.S.", "Java"},
-		{"William", "Paris", "France", "PHP"},
-		{"James", "Oslo", "Norway", "Python"},
+// Converts any type of input to string (assumed)
+func convert_to_string(s any) string {
+	// Debug code
+	//obtype := reflect.TypeOf(s)
+	//fmt.Println("obtype: ", obtype)
+
+	switch s.(type) {
+	case int, float64, string, int64, uint32:
+		b := reflect.TypeOf(s).Name()
+		//fmt.Println("b: ", b)
+		return b
+	case []string, []int, []int64, []float32, []float64:
+		b := reflect.TypeOf(s).Elem().Name()
+		//fmt.Println("s.([]string): ", s.([]string))
+		//fmt.Println("b: ", b)
+		return b
+	}
+	return "err"
+}
+
+func createFile(filename string) *os.File {
+	// Create .orc file
+	f, err := os.Create(filename)
+	check(err)
+	return f
+}
+
+func getLength(s ...interface{}) interface{} {
+	//fmt.Println("s[0]: ", s[0])
+	switch s[0].(type) {
+	case int, float64, string, int64, uint32:
+		a := 1
+		return a
+	case []string:
+		a := len(s[0].([]string))
+		return a
+	case []int:
+		a := len(s[0].([]int))
+		return a
+	case []int64:
+		a := len(s[0].([]int64))
+		return a
+	case []float32:
+		a := len(s[0].([]float32))
+		return a
+	case []float64:
+		a := len(s[0].([]float64))
+		return a
+	}
+	return "XXXX"
+}
+
+func writer(filename string, s ...interface{}) {
+	// Variable declaration
+	max_length := len(s) - 1             // amount of inputs
+	arr_length := getLength(s...).(int)  // array size of given input
+	parString := make([]string, 0)       // used to create Schema ("string1:string" etc.)
+	var res string                       // concates all strings from parString to create Schema
+	a := make([]interface{}, arr_length) // creates a duplicate of our inputs
+	copy(a, s)                           //(used to write rows for the orc file)
+
+	// Create .orc file
+	f := createFile(filename + ".orc")
+
+	// Create Schema
+	for i := 0; i <= max_length; i++ {
+		if i < max_length {
+			parString = append(parString, convert_to_string(s[i])+strconv.Itoa(i)+":"+convert_to_string(s[i])+",")
+		} else if i == max_length { // aka. the final 'string'
+			parString = append(parString, convert_to_string(s[i])+strconv.Itoa(i)+":"+convert_to_string(s[i]))
+		}
+		res += parString[i]
 	}
 
-	// Creates .orc file
-	f, err := os.Create("employee.orc")
-	check(err)
-	//fmt.Println("Checkpoint NR. 1")
+	variables := res // res = "string1:string,int1:int,string3:string,string4:string"
 
-	// set ParseSchema to the following:
-	schema, err := orc.ParseSchema("struct<string1:string,int1:int>")
+	schema, err := orc.ParseSchema("struct<" + variables + ">") // the schema is used as template to write orc file
 	check(err)
-	//fmt.Println("Checkpoint NR. 2")
 
 	w, err := orc.NewWriter(f, orc.SetSchema(schema))
 	check(err)
-	//fmt.Println("Checkpoint NR. 3")
 
-	//j := len(data_to_file)
-	//fmt.Println("length of array: ", j)
-	int1 := 0
-	for a, b := range data_to_file {
-		//fmt.Println("length of a: ", a)
-		//fmt.Println("length of b: ", b)
-		for c, d := range b {
-			int1 += 1
-			err = w.Write(data_to_file[a][c], int1)
-
-			fmt.Println(b, d)
-			// d is unnecessary, can be replaced with _ (blank identifiers)
-
-			//fmt.Println("length of c: ", c)
-			//fmt.Println("length of d: ", d)
+	for i := 0; i < arr_length; i++ {
+		for j := 0; j <= max_length; j++ {
+			switch a[j].(type) {
+			case ([]string):
+				s[j] = a[j].([]string)[i]
+				fmt.Println("s[", j, "]: ", s[j])
+			case ([]int):
+				s[j] = a[j].([]int)[i]
+				fmt.Println("s[", j, "]: ", s[j])
+			}
+		}
+		err = w.Write(s...)
+		if err != nil {
+			fmt.Println("Error (ORC.Write): ", err)
 		}
 	}
-	//fmt.Println(string1)
-	//err = w.Write(string1)
-	//check(err)
 
-	err = w.Close()
+	w.Close()
 	f.Sync()
 	f.Close()
 }
 
-func main() {
-	data()
+// Input data (for test)
+func input(x int) ([]string, []int) {
+	ERROR_MSG := []string{"XXXX"}
+	ERROR_NUM := []int{0, 0, 0, 0}
+	if x > 0 && x <= 4 {
+		if x == 1 {
+			Name := []string{"Rose", "Smith", "William", "James", "Rolf"}
+			err := []int{0}
+			return Name, err
+		}
+		if x == 2 {
+			err := []string{"0"}
+			Age := []int{28, 24, 29, 31, 21}
+			return err, Age
+		}
+		if x == 3 {
+			Country := []string{"U.K.", "U.S.", "France", "Norway", "Denmark"}
+			err := []int{0}
+			return Country, err
+		}
+		if x == 4 {
+			Skills := []string{"C", "Java", "PHP", "Python", "C++"}
+			err := []int{0}
+			return Skills, err
+		}
+	}
+	return ERROR_MSG, ERROR_NUM
+}
 
+func main() {
+	// Insert (test) data
+	Name, _ := input(1)
+	_, Age := input(2)
+	Country, _ := input(3)
+	Skills, _ := input(4)
+
+	writer("test", Name, Age, Country, Skills)
 }
